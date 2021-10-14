c0276778a3eca01aeaa4e2aa657e15c1f20177d8
Ben Becker <bforbendetta@gmail.com>
2021-10-14 15:54:04 -0700

    Clarify descriptions about the site
    The descriptions were not obvious what was going on, so I clarified them
    a little. This applies to:
    - Home page
    - Demo page
    - Draft complete page
    Issue #57

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,25 @@
     </h3>
 
     <p>
-    This page will guide you through making your very own moulage for a starting basis for pattern making custom fit garments. It will guide you through drafting a pattern based off of your own measurements.
+      This page will guide you through drafting your very own first draft moulage for making custom fit garments. It will take you step by step in the drafting process, which you will need to follow on your own.
+    </p>
+
+    <p>
+      Things you'll need:
+      <ul>
+        <li>
+          Paper at least 30" by 30"
+          <ul>
+            <li>You can a couple pages taped together</li>
+          </ul>
+        </li>
+        <li>Pencil</li>
+        <li>Eraser</li>
+        <li>Straight ruler (18-24" is suggested)</li>
+        <li>French curve (small)</li>
+        <li>French curve (large) with distances labeled</li>
+        <li>Tracing wheel</li>
+      </ul>
     </p>
 
     <p>
