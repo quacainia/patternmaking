172236ecd346fddaed4bd1ea89db74dd992055ac
Ben Becker <bforbendetta@gmail.com>
2021-10-14 22:54:49 -0700

    Add small screen notice (#72)
    This simply overlays on the entire page in the case that the window is
    too small. Since the page is currently only designed for larger screens

---
+++ src/components/Notice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="notice">
+    This page is not yet designed for small screens or mobile. Please enlarge the window or try on another device.
+  </div>
+</template>
+
+<style lang="scss">
+div.notice {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 2000;
+  background-color: #EE0;
+  font-size: 30px;
+  padding: 20px;
+  @media screen and (min-width: 1100px) {
+    display: none;
+  }
+}
+</style>
+
